1ed2c8e1ff290e0dc372940ef4395ab9fe8f43c0
Redwan Rahman <redwanrahman2002@outlook.com>
2026-03-19 22:12:51 +0600

    Add env debug to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,13 @@ set -e
 
 cd /var/www
 
+# Debug: print DB env vars to confirm they reach the container
+echo "=== ENV DEBUG ==="
+echo "DB_CONNECTION=$DB_CONNECTION"
+echo "TURSO_DB_URL=$TURSO_DB_URL"
+echo "TURSO_DB_TOKEN length=$(echo -n $TURSO_DB_TOKEN | wc -c)"
+echo "================="
+
 # Run migrations first (needs DB connection)
 php artisan migrate --force
 
