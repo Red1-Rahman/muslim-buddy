e29f5a041db3be4141232b6161324d70250ea7bf
Redwan Rahman <redwanrahman2002@outlook.com>
2026-03-20 04:24:33 +0600

    Add Google OAuth configuration and update User model for Google ID

--- config/services.php
+++ config/services.php
@@ -5,4 +5,10 @@ return [
         'recitation_id' => env('QURAN_RECITATION_ID', 7),
         'api_base' => env('QURAN_API_BASE_URL', 'https://api.quran.com/api/v4'),
     ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
 ];
