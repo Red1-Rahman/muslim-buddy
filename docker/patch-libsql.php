59762ece41747949b6861016bd6babc07759e56e
Redwan Rahman <redwanrahman2002@outlook.com>
2026-03-21 09:39:12 +0600

    Fix patch-libsql to target actual v1.0.14 driver structure

--- docker/patch-libsql.php
+++ docker/patch-libsql.php
@@ -1,22 +1,39 @@
 <?php
-$file = '/var/www/vendor/tursodatabase/turso-driver-laravel/src/Database/LibSQLPDOStatement.php';
-$content = file_get_contents($file);
-
-$old = 'return $this->statement->execute($params);';
-
-$new = '$i = 0;
-if (!empty($params) && is_int(array_key_first($params))) {
-    $this->query = preg_replace_callback("/\?/", function() use (&$i) {
-        return ":p" . $i++;
-    }, $this->query);
-    $named = [];
-    foreach (array_values($params) as $idx => $val) {
-        $named[":p$idx"] = $val;
-    }
-    $params = $named;
+$dbFile = '/var/www/vendor/tursodatabase/turso-driver-laravel/src/Database/LibSQLDatabase.php';
+if (file_exists($dbFile)) {
+    $dbContent = file_get_contents($dbFile);
+    $oldDb = 'public function prepare(string $sql): LibSQLPDOStatement
+    {
+        return new LibSQLPDOStatement(';
+    $newDb = 'public function prepare(string $sql): LibSQLPDOStatement
+    {
+        $i = 0;
+        $sql = preg_replace_callback("/\?/", function() use (&$i) {
+            return ":p" . $i++;
+        }, $sql);
+        return new LibSQLPDOStatement(';
+    
+    $dbContent = str_replace($oldDb, $newDb, $dbContent);
+    file_put_contents($dbFile, $dbContent);
+    echo "Patched LibSQLDatabase ->prepare()\n";
 }
-return $this->statement->execute($params);';
 
-$content = str_replace($old, $new, $content);
-file_put_contents($file, $content);
-echo "Patched LibSQLPDOStatement positional params\n";
+$stmtFile = '/var/www/vendor/tursodatabase/turso-driver-laravel/src/Database/LibSQLPDOStatement.php';
+if (file_exists($stmtFile)) {
+    $stmtContent = file_get_contents($stmtFile);
+    
+    $pattern = '/if \(\$this->hasNamedParameters\(\$parameters\)\) \{\s+\$this->statement->bindNamed\(\$parameters\);\s+\} else \{\s+\$this->statement->bindPositional\(array_values\(\$parameters\)\);\s+\}/';
+    
+    $replacement = 'if (!$this->hasNamedParameters($parameters)) {
+            $named = [];
+            foreach (array_values($parameters) as $idx => $val) {
+                $named[":p$idx"] = $val;
+            }
+            $parameters = $named;
+        }
+        $this->statement->bindNamed($parameters);';
+        
+    $stmtContent = preg_replace($pattern, $replacement, $stmtContent);
+    file_put_contents($stmtFile, $stmtContent);
+    echo "Patched LibSQLPDOStatement positional params\n";
+}
